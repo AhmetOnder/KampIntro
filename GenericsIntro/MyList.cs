7086b80a0a8d4bf7ef6ac7f60b5f3f00c392c217
Ahmet Önder <gaziahmetonder@gmail.com>
2021-07-08 00:16:21 +0300

    4. video sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T> // Buradaki T type i ifade ediyor. A b de olabilir.
+                    // dışarıdan T yi belirliyoruz o buraya göre çalışıyor.
+    {
+        T[] items; // aşağıdaki constraktor için. ctor yazdık ve tab tab yaptık .constraktor yaptı.
+        public MyList()
+        {
+            items = new T[0]; // MyList new lendiğinde burası çalışır.
+        }
+        public void Add(T item) // item ismi. a b veya eleman olabilir
+        {
+            T[] tempArray = items; // aşağıda newlendiğinden eskiler silinir. Silinen referans numarasıdır aslında. burada referans numarasını tutuyoruz
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        } 
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Ahmet","Mehmet","Ali","Veli"};
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //isimler = new string[5];//Burada yeni referans adresi tanımlandığından
+            //                        //yukarıdaki tanımlama geçersiz oldu
+            //isimler[4] = "Harun";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+
+            //Yukarıdaki problem burada olmaz. Bu sonradan bu problemleri çözmek için yazılmış koleksiyondur.
+            //Biz kendi koleksiyonumuzu nasıl yazarız ?
+            List<string> isimler2 = new List<string> { "Ahmet", "Mehmet", "Ali", "Veli" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Harun");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+        }
+    }
+}
